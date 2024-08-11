978c983696b8c640bc9442c3e988d136513f7069
WDD <2815582492@qq.com>
2024-08-11 18:49:06 +0800

    Update 修正 koa-compose一些错误

--- codes/programming/koa-compose.js
+++ codes/programming/koa-compose.js
@@ -2,6 +2,9 @@ const m1 = async (context, next) => {
   console.log('m1 start');
   await next()
   console.log('m1 end');
+  await next()
+  console.log('m1');
+
 }
 
 const m2 = async (context, next) => {
@@ -22,7 +25,7 @@ function compose(middlewares) {
     function dispatch(i) {
       if (i <= index) return Promise.reject(new Error(''))
 
-      let fn = middlewares.shift()
+      let fn = middlewares[i]
       if (index === middlewares.length) {
         fn = next
       }
@@ -39,3 +42,54 @@ function compose(middlewares) {
 
 const fn = compose(middlewares)
 fn()
+
+
+// 官方版
+'use strict'
+
+/**
+ * Expose compositor.
+ */
+
+module.exports = compose
+
+/**
+ * Compose `middleware` returning
+ * a fully valid middleware comprised
+ * of all those which are passed.
+ *
+ * @param {Array} middleware
+ * @return {Function}
+ * @api public
+ */
+
+function compose (middleware) {
+  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+  for (const fn of middleware) {
+    if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+  }
+
+  /**
+   * @param {Object} context
+   * @return {Promise}
+   * @api public
+   */
+
+  return function (context, next) {
+    // last called middleware #
+    let index = -1
+    return dispatch(0)
+    function dispatch (i) {
+      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+      index = i
+      let fn = middleware[i]
+      if (i === middleware.length) fn = next
+      if (!fn) return Promise.resolve()
+      try {
+        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)))
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    }
+  }
+}
